d546d0f16cd1149338c656b51dffdd893febae40
Fabio <fabiojfads@hotmail.com>
2018-07-04 20:35:20 -0300

    tipos, operadores

---
+++ ruby/hello.rb
@@ -0,0 +1,9 @@
+
+
+#puts 'Ola Ruby'
+
+puts 'Qual é o seu nome'
+nome = STDIN .gets
+#puts 'Ola ' + nome
+
+puts "Ola ' #{nome}!"
---
+++ ruby/tipos.rb
@@ -0,0 +1,19 @@
+
+
+#Ruby
+ produto = 'Notebook'
+ preco = 3000
+ quantidade = 10
+ disponivel = true
+
+
+#Ruby = tipagem dinamica
+#Variavel = recebe o valor no momento quando recebe o valor.
+
+ puts produto.class
+ puts preco.class
+ puts quantidade.class
+ puts disponivel.class
+
+
+ 
